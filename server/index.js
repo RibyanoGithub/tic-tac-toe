01e6496ae0fec3ed95fa97fd2486784f90792167
ribyano <you@example.com>
2024-06-08 01:58:05 +0800

    fixed cors issue

--- server/index.js
+++ server/index.js
@@ -15,8 +15,11 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors());
+// // Middleware
+// app.use(cors());
+app.use(cors({
+    origin: 'https://tictactoe-o9r7lh6dd-ravins-projects-8297302d.vercel.app'
+  }));
 app.use(bodyParser.json());
 
 // MongoDB connection
